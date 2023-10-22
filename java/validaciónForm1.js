362537c857720d91dfeb41a1c596a2587bfd2ce8
phlabrana <147523604+phlabrana@users.noreply.github.com>
2023-10-22 15:29:18 -0300

    Corrijo alerta gracias por suscribirte

--- java/validaciónForm1.js
+++ java/validaciónForm1.js
@@ -1,48 +1,49 @@
 function validarFormulario() {
     
-if (document.formulario.nombre.value.length < 3) {
-    alert("Por favor escriba su nombre");
-    document.formulario.nombre.focus();
-    return;
-}
-
-if(document.formulario.apellido.value.length < 3) {
-    alert("Por favor escriba su apellido");
-    document.formulario.apellido.focus();
-    return;
-}
-
-let edad = document.formulario.edad.value;
-
-edad = validarEntero(edad);
-document.formulario.edad.value = edad;
-
-if (edad == "") {
-    alert("Indique su edad en números");
-    document.formulario.edad.focus();
-    return;
+    if (document.formulario.nombre.value.length < 3) {
+        alert("Por favor escriba su nombre");
+        document.formulario.nombre.focus();
+        return;
     }
 
-if (document.formulario.mail.value == "" || document.formulario.okmail.value == "") {
-        alert("Complete ambos campos de Correo electrónico");
+    if(document.formulario.apellido.value.length < 3) {
+        alert("Por favor escriba su apellido");
+        document.formulario.apellido.focus();
+        return;
+    }
+
+    let edad = document.formulario.edad.value;
+
+    edad = validarEntero(edad);
+    document.formulario.edad.value = edad;
+
+    if (edad == "") {
+        alert("Indique su edad en números");
+        document.formulario.edad.focus();
+        return;
+        }
+
+    if (document.formulario.mail.value == "" || document.formulario.okmail.value == "") {
+            alert("Complete ambos campos de Correo electrónico");
+            document.formulario.mail.focus();
+            return;
+            }
+
+    if (document.formulario.okmail.value != document.formulario.mail.value) {
+        alert("Los email ingresados no coinciden")
         document.formulario.mail.focus();
         return;
         }
 
-if (document.formulario.okmail.value != document.formulario.mail.value) {
-    alert("Los email ingresados no coinciden")
-    document.formulario.mail.focus();
-    return;
-    }
+    if (document.formulario.pregunta.selectedIndex == 0) {
+        alert("Seleccione una respuesta para ¿Cómo nos conociste?");
+        document.formulario.pregunta.focus();
+        return;
+        }
 
-if (document.formulario.pregunta.selectedIndex == 0) {
-    alert("Seleccione una respuesta para ¿Cómo nos conociste?");
-    document.formulario.pregunta.focus();
-    return;
-    }
-}
 alert("¡Gracias por suscribirte al Club del Café!")
 document.formulario.submit();
+}
 
 function validarEntero(valor) {
         valor = parseInt(valor);
